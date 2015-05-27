0c22391580029c2112722b5df0e63437fa9b4496
Deizu <contact@deasrichardson.com>
2015-05-27 14:38:33 -0400

    Fixed 2nd level sorting issues and refactored. Good to go! Passed all tests.

--- rankall.R
+++ rankall.R
@@ -4,9 +4,11 @@ rankall <- function(outcome, num = "best"){
   outcometable <- read.csv("outcome-of-care-measures.csv", 
                            na.strings="Not Available",
                            stringsAsFactors=FALSE)
+  
   outcomecheck <- c("heart attack", "heart failure", "pneumonia") # Outcomes
   statecheck <- as.character(unique(outcometable$State)) # States
   statecheck <- statecheck[order(statecheck)] # States in A-Z order
+  allstates <- data.frame(hospital=NA,state=statecheck) # Data frame to alter
   outcometable[,11] <- as.numeric(outcometable[,11]) # 30 day heart atck drate
   outcometable[,17] <- as.numeric(outcometable[,17]) # 30 day heart fail drate
   outcometable[,23] <- as.numeric(outcometable[,23]) # 30 day pneumonia drate
@@ -20,56 +22,55 @@ rankall <- function(outcome, num = "best"){
     stop("invalid outcome")
   }
   
-  allstates <- data.frame()
-  
-  ## For each state, find the hospital of the given rank
-  
   for(i in c(1:length(statecheck))) {
     
-  stateresults <- subset(outcometable, State==statecheck[i])
-  
-  if(outcome == "heart attack") {
-    stateresults <- stateresults[complete.cases(stateresults[,c(1:3,4)]),c(1:3,4)]
-  }
-  if(outcome == "heart failure") {
-    stateresults <- stateresults[complete.cases(stateresults[,c(1:3,5)]),c(1:3,5)]
-  }
-  if(outcome == "pneumonia") {
-    stateresults <- stateresults[complete.cases(stateresults[,c(1:3,6)]),c(1:3,6)]
-  }
-  
-  if(class(num)=="character"){
-    if(num == "best"){
-      rval <- 1
-    } else if(num == "worst"){
-      rval <- nrow(stateresults)
-    } else {
+    stateresults <- subset(outcometable, State==statecheck[i])
+    
+    if(outcome == "heart attack") {
+      stateresults <- stateresults[complete.cases(stateresults[,c(1:3,4)]),c(1:3,4)]
+    }
+    if(outcome == "heart failure") {
+      stateresults <- stateresults[complete.cases(stateresults[,c(1:3,5)]),c(1:3,5)]
+    }
+    if(outcome == "pneumonia") {
+      stateresults <- stateresults[complete.cases(stateresults[,c(1:3,6)]),c(1:3,6)]
+    }
+    
+    stateresults <- stateresults[order(stateresults[,4],stateresults[,2]),]
+    
+    if(class(num)=="character"){
+      if(num == "best"){
+        rval <- 1
+      } else if(num == "worst"){
+        rval <- nrow(stateresults)
+      } else {
+        rval <- NA
+      }
+    }
+    
+    if(class(num)=="numeric"){
+      if(num > 0 && num <= nrow(stateresults)) {
+        rval <- num
+      } else if(num > nrow(stateresults) | num < 1){
+        rval <- NA
+        stateresults[1,statecheck[i]]<-NA
+      } else {
+        rval <- NA
+      }
+    }
+    
+    if((class(num)=="numeric"|class(num)=="character")==FALSE){
       rval <- NA
     }
-  }
-  
-  if(class(num)=="numeric"){
-    if(num > nrow(stateresults) | num < 1){
-      message("No such animal here.")
-      rval <- 1
-      stateresults[1,statecheck[i]]<-NA
-    } else if(num > 0 && num < nrow(stateresults)) {
-      rval <- num
-    } else {
-      rval <- NA
+    
+    if(!is.na(rval)){
+      allstates[i,1] <- stateresults[rval,2]
+    } else if(is.na(rval)){
+      allstates[i,1] <- NA
     }
-  }
-  
-  if((class(num)=="numeric"|class(num)=="character")==FALSE){
-    rval <- NA
-  }
-  
-  allstates <- rbind(allstates, c(stateresults[rval,2],statecheck[i]))
-  
   }
   
   ## Return a data frame with the hospital names and the
   ## abbreviated state names
-  colnames(allstates) <- c("Hospital","State")
   allstates
 }
--- workingspace.R
+++ workingspace.R
@@ -1,5 +1,6 @@
 ## Part 1: Plot the 30-day mortality rates for heart attack
 
+partone <- function(){
 # Read in CSV of outcome data into data frame.
 outcome <- read.csv("outcome-of-care-measures.csv")
 
@@ -10,21 +11,22 @@ outcome[,11] <- as.numeric(outcome[,11])
 # Plot histogram of the 30 day mortality rates for heart attack.
 hist(outcome[,11], xlab="Mortality Rate", ylab="Frequency", 
      main="30-Day Mortality Rate for Heart Attack")
+}
 
 ###############################################################################
 
 ## Part 2: Finding the best hospital in a state
 
-source("best.R")
+#source("best.R")
 
 ###############################################################################
 
 ## Part 3: Ranking hospitals by outcome in a state
 
-source("rankhospital.R")
+#source("rankhospital.R")
 
 ###############################################################################
 
 ## Part 4: Ranking hospitals by outcome in a state
 
-source("rankall.R")
+#source("rankall.R")
